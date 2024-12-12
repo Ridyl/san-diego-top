16e08958156c7a7310d3d6831823dd99acd71024
Ridyl <joey.dalatri214@gmail.com>
2024-12-12 15:49:28 -0500

    Added JS functionality to properly pull and display data.json file.

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-$(document).ready(function() {
-// write your code here
-
-});
+document.addEventListener("DOMContentLoaded", (function() {
+    let table = document.getElementById("table");
+    let tableBody = document.getElementById("table-body");
+    fetch("data.json")
+        .then(response => response.json())
+        .then(json => {
+            const data = json;
+            for(let location in Object.keys(data)) {
+                let newRow = document.createElement("tr");
+                for(let key in data[location]) {
+                    let rowData = document.createElement("td");
+                    let text = document.createTextNode(data[location][key]);
+                    console.log(data[location][key]);
+                    rowData.appendChild(text);
+                    newRow.appendChild(rowData);
+                }
+                tableBody.appendChild(newRow);
+            }
+            table.appendChild(tableBody);
+        });
+    })
+);
