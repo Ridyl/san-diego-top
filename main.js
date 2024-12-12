2d2f7f5d9e86669ac7487deecfdc7044d66dd897
Ridyl <joey.dalatri214@gmail.com>
2024-12-12 16:02:55 -0500

    Added comments for clarity on what each section of code does and the order of which everything is applied

--- main.js
+++ main.js
@@ -4,18 +4,28 @@ document.addEventListener("DOMContentLoaded", (function() {
     fetch("data.json")
         .then(response => response.json())
         .then(json => {
+            // assigns data to json value.
             const data = json;
+            // iterates through each location in data array
             for(let location in Object.keys(data)) {
+                // creates a new row for each location --- data[index]
                 let newRow = document.createElement("tr");
+                // iterates through each value within each location object that is in the data array --- data[index][key]
                 for(let key in data[location]) {
+                    // creates a new cell for each item within object
                     let rowData = document.createElement("td");
+                    // creates text from extracted key values --- "name" "description" "location" values
                     let text = document.createTextNode(data[location][key]);
-                    console.log(data[location][key]);
+
+                    // appends text to rd
                     rowData.appendChild(text);
+                    // appends rd to whole row
                     newRow.appendChild(rowData);
                 }
+                // appends all created rows with appropriate ammount of cells and text
                 tableBody.appendChild(newRow);
             }
+            // appends table body to the table
             table.appendChild(tableBody);
         });
     })
