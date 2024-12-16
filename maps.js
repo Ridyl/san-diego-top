46a07d7f56b211a4663093b4b02755dc310798ca
Ridyl <joey.dalatri214@gmail.com>
2024-12-16 14:57:41 -0500

    All map logic is put in place. Allows for opening of new maps, opening and closing of the same map from the same button. Added a pin that is placed in center of map for location clicked.

--- maps.js
+++ maps.js
@@ -1,27 +1,97 @@
 // selects all added buttons added from main.js
 let buttons = document.querySelectorAll("#map-button");
 // current state of a map on the page. Set false until first button click
-let open = false;
+let click = 0;
+let lastCoords = [];
+
+// leafletjs icon creation
+let blueIcon = L.icon({
+    iconUrl: 'images/blue-pin.png',
+
+    iconSize:     [26, 40], // size of the icon
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 buttons.forEach(button => {
     button.addEventListener("click", function(event) {
-        // if there are no maps open one will open
-        if(open == false) {
-            let mapRow = document.createElement("div");
-            // access row of button press
-            let parentRow = this.parentNode.parentNode.parentNode;
-            console.log(parentRow);
-            parentRow.appendChild(mapRow);
-            // console.log(this.className);
-            // console.log("a map has been opened");
-        // if there is an open map it will be closed and selected one will be opened
-        } else if (open == true) {
-            // console.log("a map has been closed");
-            // console.log(this.className);
-            // console.log("a new map has been opened");
+        // sets closer value to current click value to keep value of previous maps id = "#"
+        let closer = click;
+        let old = document.getElementById(`${closer}`);
+        click++;
+
+        // access row of button press
+        let parentRow = this.parentNode.parentNode;
+
+        // create new table row and set id
+        let mapRow = document.createElement("tr");
+        mapRow.setAttribute("id", click);
+
+        // access pressed buttons class name where location data is stored and returns the string as a split array
+        let coords = this.className.split(',');
+
+        // creates table data with proper attributes
+        let mapData = document.createElement("td");
+        mapData.colSpan = "3";
+        mapData.setAttribute("id", "map");
+
+        // adds data to created row then adds row after existing
+        mapRow.appendChild(mapData);
+        parentRow.after(mapRow);
+
+        // if the first button is pressed
+        if (click == 1) {
+            let map = L.map('map').setView(coords, 15);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+        
+            L.marker(coords, {icon: blueIcon}).addTo(map);
+
+        // if the same button is pressed
+        } else if (coords.toString() == lastCoords.toString()) {
+            // if old has not been removed yet
+            if (old != null) {
+                while (old.firstChild) {
+                    old.removeChild(old.lastChild);
+                }
+                old.remove();
+                mapRow.remove(); 
+
+                // resets lastCoords so on next button press comparisons don't end up back here
+                lastCoords = [];
+            // else redraw map of previously closed map
+            } else {
+                let map = L.map('map').setView(coords, 15);
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+            
+                L.marker(coords, {icon: blueIcon}).addTo(map); 
+            }
+        
+        // if not first press and button is different than previous
+        } else if (click > 1) {
+
+            // if the button that was pressed is not the same as previous AND previous has not been removed
+            if(coords.toString() != lastCoords.toString() && old != null) {
+                // removes all children of last created row and then removes itself
+                while (old.firstChild) {
+                    old.removeChild(old.lastChild);
+                }
+                old.remove();
+            }
+        
+            let map = L.map('map').setView(coords, 15);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+        
+            L.marker(coords, {icon: blueIcon}).addTo(map); 
         }
-        open = true;
+        // saves coords for closing
+        lastCoords = coords;
     });
 });
-
-// need to find a way to access table to draw a new table row under the row that the button was pressed on.
